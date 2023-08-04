97ce81d59a906d6c7525b675a6ea074962ae6188
AbhiGulhane9 <abhijitgulhane007@gmail.com>
2023-08-03 23:42:29 -0700

    [AbhiGulhane9] Added New Test Case

---
+++ src/test/java/bookingSites/Paytm.java
@@ -0,0 +1,18 @@
+package bookingSites;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Paytm {
+  @Test
+  public void Paytmtest() throws InterruptedException 
+  {
+	  WebDriver driver = new ChromeDriver();
+	  Reporter.log("Opening Paym", true);
+	  driver.get("https://tickets.paytm.com/flights/");
+	  Thread.sleep(3000);
+	  driver.close();
+  }
+}
